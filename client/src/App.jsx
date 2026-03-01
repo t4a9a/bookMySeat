a5ff8410858a5eb45dcc2092c90ab1c9cd161208
t4a9a-kali <thapasinghvaibhav@gmail.com>
2026-03-01 13:21:06 -0500

    add folder and routes to the app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import { Home } from 'lucide-react'
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import MovieDetails from './pages/MovieDetails'
 
 const App = () => {
+  
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+  
   return (
-    <div>
-      
-    </div>
+    <>
+        { !isAdminRoute && <Navbar/> }
+        <Routes>
+          <Route path= '/' element = {<Home/>} />
+          <Route path= '/movies' element = {<Movies/>} />
+          <Route path= '/movies/:id' element = {<MovieDetails/>} />
+          <Route path= '/movies/:id/:date' element = {<SeatLayout/>} />
+          <Route path= '/my-bookings' element = {<MyBookings/>} />
+          <Route path= '/favorite' element = {<Favorite/>} />
+        </Routes>
+    </>
   )
 }
